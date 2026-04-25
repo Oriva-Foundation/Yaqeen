c127a01e8367b39d91d2f94cc6d51993536ae864
mahmoudsruby <mahmoudsruby@gmail.com>
2026-04-26 01:27:08 +0300

    Fix the about page lmports in the header

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,5 @@
-import Header from '../../components/header.jsx';
-import Footer from '../../components/footer.jsx';
+import Header from '../components/header.jsx';
+import Footer from '../components/footer.jsx';
 
 export default function AboutPage() {
   return (
